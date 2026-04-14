a625a14b040c65e4a19d4464b63403b37209f8f6
exmanek <szymon.podlasiak.111@gmail.com>
2026-04-14 16:34:18 +0200

    change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,40 @@
 import GradientBackground from "./ui/GradientBackground";
-import ASCIIText from './ui/ASCIIText';
-
+import SplitText from "./ui/SplitText";
+import TextType from './ui/TextType';
 
 const Header = () => {
     return (
         <header className="header">
             <GradientBackground />
             <div className="header__content">
-                <h2 className="header__heading">Szymon Podlasiak</h2>
-                <p className="header__text">Web Developer</p>
+                <SplitText
+                text="Szymon Podlasiak"
+                className="header__heading"
+                delay={50}
+                duration={1.25}
+                ease="power3.out"
+                splitType="chars"
+                from={{ opacity: 0, y: 40 }}
+                to={{ opacity: 1, y: 0 }}
+                threshold={0.1}
+                rootMargin="-100px"
+                textAlign="center"
+                showCallback
+                />
+                <TextType 
+                className="header__text"
+                text={["Frontend Developer", "for your website!", "Contact Now"]}
+                typingSpeed={75}
+                pauseDuration={1500}
+                showCursor
+                cursorCharacter="_"
+                texts={[""]}
+                deletingSpeed={50}
+                variableSpeedEnabled={false}
+                variableSpeedMin={60}
+                variableSpeedMax={120}
+                cursorBlinkDuration={0.5}
+                />
             </div>
         </header>
     )
